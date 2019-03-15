8404844236241385944d6170937537f740ffef07
JAEHO SONG <thruthesky@gmail.com>
2019-03-16 01:41:12 +0800

    update for

--- sw.js
+++ sw.js
@@ -1,18 +1,19 @@
-self.addEventListener('install', function(e) {
+self.addEventListener('install', function (e) {
     e.waitUntil(
-      caches.open('video-store').then(function(cache) {
-        return cache.addAll([
-          '/images/icons/icon-72x72.png'
-        ]);
-      })
+        caches.open('video-store').then(function (cache) {
+            return cache.addAll([
+                '/start.html',
+                '/images/icons/icon-72x72.png'
+            ]);
+        })
     );
-   });
-   
-   self.addEventListener('fetch', function(e) {
-     console.log(e.request.url);
-     e.respondWith(
-       caches.match(e.request).then(function(response) {
-         return response || fetch(e.request);
-       })
-     );
-   });
+});
+
+self.addEventListener('fetch', function (e) {
+    console.log(e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(function (response) {
+            return response || fetch(e.request);
+        })
+    );
+});
