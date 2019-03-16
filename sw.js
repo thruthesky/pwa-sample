865a0193068029e51f66b052baa359a850bd0832
JAEHO SONG <thruthesky@gmail.com>
2019-03-17 01:53:51 +0800

    new

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 /**
  * PWA service worker.js
  * 
- * @version
+ * @version 1:51 am
  */
 var myCache = 'pwa-cache-2';
 self.addEventListener('install', function (e) {
@@ -12,17 +12,17 @@ self.addEventListener('install', function (e) {
             ]);
         })
     );
-    // event.waitUntil(
-    //     caches.keys().then(function (cacheNames) {
-    //         return Promise.all(
-    //             cacheNames.map(function (cacheName) {
-    //                 if (myCache != cacheName) {
-    //                     return caches.delete(cacheName);
-    //                 }
-    //             })
-    //         );
-    //     })
-    // );
+    event.waitUntil(
+        caches.keys().then(function (cacheNames) {
+            return Promise.all(
+                cacheNames.map(function (cacheName) {
+                    if (myCache != cacheName) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', function (e) {
